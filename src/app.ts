3e9ca8e64d7f29a491c54ca52a5ffacad9c328bf
Manueldy <a20210360@utem.edu.mx>
2024-10-18 14:43:20 -0600

    Corrections of spelling mistakes and arrangement of routes in app.ts.

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,14 @@ import express, { Express, NextFunction, Request, Response } from "express";
 import cors from "cors";
 import bodyParser from 'body-parser';
 
-import morgan from 'morgan'
+import morgan from 'morgan';
 import { notFoundHandler } from "./middlewares/notFound";
-import userRouter from './routes/user.router'
-import stationRouter from './routes/station.router'
+import userRouter from './routes/user.router';
+import stationRouter from './routes/station.router';
+import bikeRouter from './routes/bike.router';
+import rentalRouter from './routes/rental.router';
+import maintenanceRouter from "./routes/maintenance.router";
+
 const app: Express = express();
 
 //configuring express 
@@ -19,7 +23,11 @@ app.set("trust proxy", ["loopback", "linklocal", "uniquelocal"]);
 
 //routes
 app.use('/users',userRouter);
-app.use('/stations',stationRouter)
+app.use('/stations',stationRouter);
+app.use('/rentals',rentalRouter);
+app.use('/maintenances', maintenanceRouter);
+app.use('/bikes', bikeRouter);
+
 
 app.all('*', notFoundHandler);
 
--- src/routes/bike.router.ts
+++ src/routes/bike.router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { BikesController } from '../controllers/bike.controller}';
+import { BikesController } from '../controllers/bike.controller';
 
 const router = Router();
 
