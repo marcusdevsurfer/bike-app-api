8127d09825a81df5f722ac44bdd0bb6e499e3776
rrbs20210330 <a20210330@utem.edu.mx>
2024-10-17 18:54:43 -0600

    Bike router and controller

---
+++ src/controllers/bike.controller}.ts
@@ -0,0 +1,61 @@
+import { Request, Response } from 'express';
+import Bike from '../models/Bike';
+export class BikesController {
+
+    static async getAllBikes(req: Request, res: Response) {
+        Bike.find().then((result)=>{
+            return res.json(result);
+        }).catch((err)=>{
+            return res.status(500).json({ message: err.message });
+        });
+    }
+
+    static async getBikeById (req: Request, res: Response) {
+        const bikeId = req.params.bikeId;
+        Bike.findById(bikeId).then((result)=>{
+            if (!result) return res.status(404).json({ message: 'Bike not found' });
+            return res.status(200).json(result);
+        }).catch(err => {
+            res.status(500).json({ message: err.message });
+        });
+    }
+
+
+
+    static async createBike(req: Request, res: Response) {
+        const bike = new Bike({
+            serialNumber: req.body.serialNumber,
+            status: req.body.status,
+            model: req.body.model,
+            station: req.body.station
+
+        });
+        
+        try {
+            const newBike = await bike.save();
+            res.status(201).json(newBike);
+        } catch (err:any) {
+            res.status(400).json({ message: err.message });
+        }
+    }
+
+    static async updateBike(req: Request, res: Response) {
+        const bikeId = req.params.bikeId;
+        Bike.findByIdAndUpdate(bikeId, req.body, { new: true }).then(result =>{
+            if (!result) return res.status(404).json({ message: 'Bike not found'});
+            return res.status(200).json(result);
+        }).catch(err =>{
+            res.status(500).json({ message: err.message });
+        })
+    }
+
+    static async deleteBike(req: Request, res: Response)  {
+        const bikeId = req.params.bikeId;
+        Bike.findByIdAndDelete(bikeId).then(result =>{
+            if (!result) return res.status(404).json({ message: 'Bike not found'});
+            return res.status(200).json({ message: 'Bike deleted successfully' });
+        }).catch(err =>{
+            res.status(500).json({ message: err.message });
+        })
+    }
+}
---
+++ src/routes/bike.router.ts
@@ -0,0 +1,21 @@
+import { Router } from 'express';
+import { BikesController } from '../controllers/bike.controller}';
+
+const router = Router();
+
+// Obtener todos los usuarios
+router.get('/', BikesController.getAllBikes);
+
+// Obtener un usuario por userId
+router.get('/:userId', BikesController.getBikeById);
+
+// Crear un nuevo usuario
+router.post('/', BikesController.createBike);
+
+// Actualizar un usuario
+router.put('/:userId', BikesController.updateBike);
+
+// Eliminar un usuario
+router.delete('/:userId', BikesController.deleteBike);
+
+export default router;
